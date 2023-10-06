8091e22e84c4b53a5d02c40950f6df9bd444555b
Kartik <s11kartik2111087@gmail.com>
2023-10-06 23:05:43 +0530

    States

--- state/src/App.js
+++ state/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useState} from 'react'
 
 const App = () => {
+  
+  // const [isImportant, setIsImportant] = React.useState("Yes")
+
+    const [isImportant, setIsImportant] = useState("Yes")
+  
+  function handleclick() {
+    setIsImportant("No")
+  }
+
   return (
     <div>
-      
+      <button onClick={handleclick}>{isImportant}</button>
     </div>
   )
 }
