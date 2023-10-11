591fe6913ecc745b22c802eb5725495c01c31923
Kartik <s11kartik2111087@gmail.com>
2023-10-11 19:41:06 +0530

    Conditionalrendering

--- jokesprops/src/Jokes.jsx
+++ jokesprops/src/Jokes.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Jokes.css'
 
-const Jokes = ({img, heading}) => {
+const Jokes = ({ img, heading }) => {
+  const [isShown, setIsShown] = React.useState(false)
+  
+  function toggle() {
+    setIsShown( !isShown)
+  }
   return (
     <div className='jump'>
-          <img src={ img } alt='Jokes'/>
-          <h2>
-            {heading}  
-          </h2>
+      <img src={img} alt='Jokes' />
+      {/* Important */}
+      {isShown && <h2> {heading} </h2>}
+      <button onClick={toggle}> Joke </button>
     </div>
   )
 }
