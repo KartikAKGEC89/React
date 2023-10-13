72781d6a8d01969624dabacdf10b4e4850806d64
Kartik <s11kartik2111087@gmail.com>
2023-10-13 20:18:25 +0530

    Formmanage

--- forms/src/Form.jsx
+++ forms/src/Form.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 
 const Form = () => {
-    const [name, setName] = React.useState("")
-    const [ lastName, setLastName] = React.useState("")
+    const [name, setName] = React.useState({firstname: "", lastName: "", email:""})
+    // const [ lastName, setLastName] = React.useState("")
     console.log(name)
-    console.log(lastName)
+    // console.log(lastName)
 
     function handledata(event) {
-        setName(event.target.value)
+      setName(prevname => {
+        // return prevname = {
+        //   ...prevname,
+        //   [event.target.name]: event.target.value
+        // }
+        return {
+          ...prevname,
+          [event.target.name]: event.target.value
+          }
+        })
     }
 
-    function handlelast(event) {
-        setLastName(event.target.value)
-    }
+    // function handlelast(event) {
+    //     setLastName(event.target.value)
+    // }
   return (
     <div>
-          <input type='text' placeholder='name' onChange={handledata} /> 
-          <input type='text' placeholder='last name' onChange={handlelast} />
+      <input type='text' placeholder='name' onChange={handledata} name='firstname' value={name.firstname}/> 
+      <input type='text' placeholder='lastname' onChange={handledata} name='lastName' value={name.lastName}/>
+      <input type='email' placeholder='email' onChange={handledata} name='email' value={name.email} />
     </div>
   )
 }
