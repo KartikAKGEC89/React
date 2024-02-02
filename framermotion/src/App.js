a2c40312011f3329561074a2ddfd3e9cd43a50af
Kartik <s11kartik2111087@gmail.com>
2024-02-02 14:39:56 +0530

    feat: ScrollBar

--- framermotion/src/App.js
+++ framermotion/src/App.js
@@ -3,10 +3,27 @@ import Textanimation from './components/textanimation.jsx'
 import Boxanimation from './components/boxanimation.jsx'
 import Dragableanimation from './components/Dragableanimation.jsx'
 import Multiplecomponent from './components/Multiplecomponent.jsx'
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const App = () => {
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress);
+  
   return (
     <>
+      <motion.div style={{
+        scaleX,
+      position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  height: 10,
+  background: "red",
+  transformOrigin: "0%"}} />
+      <h1>
+        <code>useScroll</code> with spring smoothing
+      </h1>
       <h1>Framer Motion Package use</h1>
       <Textanimation />
       <Boxanimation />
