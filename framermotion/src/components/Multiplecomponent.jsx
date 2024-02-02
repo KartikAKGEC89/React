a8f14036a8b4196eb183aa500b625f7607a7f4e0
Kartik <s11kartik2111087@gmail.com>
2024-02-02 14:01:13 +0530

    feat: New modal

--- framermotion/src/components/Multiplecomponent.jsx
+++ framermotion/src/components/Multiplecomponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -13,6 +13,21 @@ const container = {
   }
 };
 
+const items = [{
+    id: 1,
+    subtitle: "hello",
+    title:"Kartik"
+},
+{
+    id: 2,
+    subtitle: "hello",
+    title:"Raman"
+},{
+    id: 3,
+    subtitle: "hello",
+    title:"Mohan"
+},]
+
 const item = {
   hidden: { y: 20, opacity: 0 },
   visible: {
@@ -22,6 +37,9 @@ const item = {
 };
 
 const Multiplecomponent = () => {
+
+    const [selectedId, setSelectedId] = React.useState(null)
+
   return (
       <>
     <motion.ul
@@ -43,7 +61,26 @@ const Multiplecomponent = () => {
       <motion.li key={index} className="item" variants={item} style={{background: "white",
   radius: "100"}}/>
     ))}
-  </motion.ul>
+          </motion.ul>
+          
+
+           
+{items.map(item => (
+  <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
+    <motion.h5>{item.subtitle}</motion.h5>
+    <motion.h2>{item.title}</motion.h2>
+  </motion.div>
+))}
+
+<AnimatePresence>
+  {selectedId && (
+    <motion.div layoutId={selectedId}>
+      <motion.h5>{item.subtitle}</motion.h5>
+      <motion.h2>{item.title}</motion.h2>
+      <motion.button onClick={() => setSelectedId(null)} />
+    </motion.div>
+  )}
+</AnimatePresence>
     </>
   )
 }
